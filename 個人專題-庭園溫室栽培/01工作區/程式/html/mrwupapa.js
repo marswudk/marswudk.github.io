ea0022dc26954b2136f1ef43ab862e1e42c4868e
marswudk <emeqcoindo@gmail.com>
2019-12-22 23:01:48 +0800

    try one page

--- 個人專題-庭園溫室栽培/01工作區/程式/html/mrwupapa.js
+++ 個人專題-庭園溫室栽培/01工作區/程式/html/mrwupapa.js
@@ -1,9 +1,47 @@
 // bootstrap
 // modal
 $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+  $('#myInput').trigger('focus')
+})
 
 
 
 
+var number_li = $('.nav li').length
+n = 1
+
+$(window).on('mousewheel', function (e) {
+  // console.log(e.deltaY);
+  if (e.deltaY == -1) {
+    if (n < number_li) {
+      n++
+      $(`.page:nth-child(${n})`).addClass('remove')
+      $('.nav li').removeClass('active')
+      $(`.nav li:nth-child(${n})`).addClass('active')
+
+    }
+  } else {
+    if (n > 1) {
+      n--
+      $(`.page:nth-child(${n + 1})`).removeClass('remove')
+      $('.nav li').removeClass('active')
+      $(`.nav li:nth-child(${n})`).addClass('active')
+
+    }
+  }
+  console.log("n:",n,"number_li",number_li);
+
+
+})
+
+
+$('.nav li').click(function () {
+
+  $('.nav li').removeClass('active')
+  $(this).addClass('active')
+  var index = $(".nav li").index(this);
+  $(`.page:nth-child(${index + 2})`).removeClass('remove')
+  $(`.page:nth-child(${index + 1})`).addClass('remove')
+
+  console.log(index);
+})
